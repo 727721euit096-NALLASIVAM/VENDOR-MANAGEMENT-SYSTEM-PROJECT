2d457e73444c66c2226ae634a4638dc22c935d64
727721euit096-NALLASIVAM <125950986+727721euit096-NALLASIVAM@users.noreply.github.com>
2023-07-20 10:13:42 +0530

    Add files via upload

---
+++ Day-9/src/App.js
@@ -0,0 +1,38 @@
+import React,{useState} from "react";
+import './App.css';
+import {  Routes, Route } from "react-router-dom";
+import Home from "./home";
+import Forms from "./Register";
+import Login from "./Login";
+import Products from "./products";
+import AboutPage from "./AboutPage";
+import { Feedback } from "./feedback";
+
+
+function App() {
+return (
+    <div>
+    <div className="main">
+    <footer/>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/login" element={<Login/>}></Route>
+    <Route path="/reg" element={<Forms/>}></Route>
+    <Route path="/feedback" element={<Feedback/>}></Route>
+    <Route path="/products" element={<Products/>}></Route>
+    <Route path="/AboutPage" element={<AboutPage/>}></Route>
+
+  </Routes>
+  </div>
+ 
+
+  
+
+ 
+    </div>
+  );
+}
+
+
+
+export default App;
